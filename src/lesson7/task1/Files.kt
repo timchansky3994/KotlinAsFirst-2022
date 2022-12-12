65ebe984f03dd9ec1360e9a213d5668f61e3c47f
timchansky3994 <timurall@yandex.ru>
2022-12-13 01:18:02 +0300

    fixed centerFile

--- src/lesson7/task1/Files.kt
+++ src/lesson7/task1/Files.kt
@@ -122,12 +122,14 @@ fun sibilants(inputName: String, outputName: String) {
 fun centerFile(inputName: String, outputName: String) {
     val writer = File(outputName).bufferedWriter()
     val lines = File(inputName).readLines()
-    val longest = lines.maxOf { it.length }
-    for (line in lines) {
-        val trimmed = line.trim(' ')
-        writer.write(" ".repeat((longest - trimmed.length) / 2))
-        writer.write(trimmed)
-        writer.newLine()
+    if (lines.isNotEmpty()) {
+        val longest = lines.maxOf { it.length }
+        for (line in lines) {
+            val trimmed = line.trim(' ')
+            writer.write(" ".repeat((longest - trimmed.length) / 2))
+            writer.write(trimmed)
+            writer.newLine()
+        }
     }
     writer.close()
 }
